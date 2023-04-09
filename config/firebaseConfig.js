6087bea3aa5404315c3cd5619a8d8d5f8b584fcf
JorgePeju <123939712+JorgePeju@users.noreply.github.com>
2023-04-09 21:35:28 +0200

    añadida fecha y principio de firebase

---
+++ config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDp8hIVwDGpovnTMu1Ag5wuMFoeqqvjyxQ",
+    authDomain: "proyecto-blog-9520e.firebaseapp.com",
+    projectId: "proyecto-blog-9520e",
+    storageBucket: "proyecto-blog-9520e.appspot.com",
+    messagingSenderId: "129435450828",
+    appId: "1:129435450828:web:c05e0797bd61a30185f169"
+  };
+
+
+  module.exports={firebaseConfig}
--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -1,3 +1,8 @@
+const { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendPasswordResetEmail,  } = require('firebase/auth');
+const { authFb } = require('../helpers/firebase')
+
+
+
 
 const formSignIn = async (req, res) => {
 
@@ -5,7 +10,54 @@ const formSignIn = async (req, res) => {
 
 };
 
+const signInCreate = async (req, res) => {
 
+    const { email, password } = req.body
+    
+    console.log(email, password)
+    
+    try {
+
+        const userCredentials = await signInWithEmailAndPassword(authFb, email, password)
+       
+          
+       
+    } catch (error) {
+
+        if (error.code === 'auth/wrong-password') {
+
+             res.render('../views/signInForm.ejs', {
+                error
+             });
+
+        } else if (error.code === 'auth/user-not-found') {
+
+            res.render('../views/signInForm.ejs', {
+                error
+             });
+
+        } else {
+
+           res.render('../views/signInForm.ejs', {
+                error
+             });
+
+        }
+    }
+}
+
+const logOut = async (req, res) => {
+
+    try {
+
+        await signOut(authFb)
+        console.log("signup out of ");
+
+    } catch (error) {
+
+        console.log(error)
+    }
+}
 
 
 
