dee45cee44b4ae2b19408378949b3e498eec834c
JorgePeju <123939712+JorgePeju@users.noreply.github.com>
2023-04-11 22:58:53 +0200

    documentacion añadida

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -3,10 +3,12 @@ const { getAuth } = require('firebase/auth');
 const { firebaseConfig } = require('../config/firebaseConfig')
 
 /**
+ * Instancia de Firebase.
  * @type {Object} 
  */
 const firebaseApp = initializeApp(firebaseConfig);
 /**
+ * Instancia de la autentificación de Firebase
  * @type {Object} 
  */
 const authFb = getAuth(firebaseApp)
